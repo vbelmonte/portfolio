08c2ebedbf829bc18991c05ee4d76168460be243
Villy Belmonte <vjbbelmont@gmail.com>
2023-12-15 19:52:49 -0800

    Modify properties for skills and expertise section to make it responsive

--- src/components/SoftSkillCard.jsx
+++ src/components/SoftSkillCard.jsx
@@ -1,7 +1,12 @@
+import chevron from '../assets/chevron.svg'
+
 export function SoftSkillCard({ skill, description }) {
   return (
     <div className='soft-skill-listing'>
-      <h6>{skill}</h6>
+      <div className='soft-skill-name'>
+        <h6>{skill}</h6>
+        <img className='chevron' src={chevron} />
+      </div>
       <p>{description}</p>
     </div>
   )
