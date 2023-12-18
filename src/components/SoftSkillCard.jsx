d34b0325a63fd5dec15a6c835ebf648662b9cf97
Villy Belmonte <vjbbelmont@gmail.com>
2023-12-18 14:30:09 -0800

    Remove chevron svg and clean up SoftSkillCard component and properties

--- src/components/SoftSkillCard.jsx
+++ src/components/SoftSkillCard.jsx
@@ -1,11 +1,8 @@
-import chevron from '../assets/chevron.svg'
-
 export function SoftSkillCard({ skill, description }) {
   return (
     <div className='soft-skill-listing'>
       <div className='soft-skill-name'>
         <h6>{skill}</h6>
-        <img className='chevron' src={chevron} />
       </div>
       <p>{description}</p>
     </div>
